c2230e8200e1db142813e7494ad80b31fa9c2a3f
Enzo Palermo <118272358+Enzobu@users.noreply.github.com>
2026-08-11 22:56:59 +0200

    feat: route invitation links to dedicated signup

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { AdminMailTest } from "./AdminMailTest.js";
 import { App } from "./App.js";
+import { InvitationAccept } from "./InvitationAccept.js";
 import "./styles.css";
 import "./admin-mail-test.css";
 
@@ -12,9 +13,17 @@ if (!root) {
   throw new Error("Root element not found");
 }
 
+const isInvitationRoute = window.location.pathname === "/invitations/accept";
+
 createRoot(root).render(
   <StrictMode>
-    <App />
-    <AdminMailTest />
+    {isInvitationRoute ? (
+      <InvitationAccept />
+    ) : (
+      <>
+        <App />
+        <AdminMailTest />
+      </>
+    )}
   </StrictMode>,
 );
